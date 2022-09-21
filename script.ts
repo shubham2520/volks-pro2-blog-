5dd043d3d883caea95e693fbc0061341dd9b93ff
Shubham1512 <79195479+shubham2520@users.noreply.github.com>
2022-09-21 16:40:31 +0530

    Add files via upload

---
+++ script.ts
@@ -0,0 +1,21 @@
+var img = document.getElementById("gfg-img");
+var preview = document.querySelector(".zoom-preview") as HTMLElement | null;
+
+//calculating the ratio by which we want to magnify the image
+//you can increase or decrease it according to your requirement
+var x = preview?.offsetWidth / 100;
+var y = preview?.offsetHeight / 100;
+
+img?.addEventListener("mousemove", (e) => {
+    preview.style.backgroundImage = 
+"url(wp8277808-kohinoor-wallpapers.jpg)";
+    preview?.style.backgroundSize = img?.width * x + 
+      "px " + img?.height * y + "px";
+    var posX = e.offsetX;
+    var posY = e.offsetY;
+    preview.style.backgroundPosition = "-" + posX * x + 
+      "px -" + posY * y + "px";
+});
+img?.addEventListener("mouseout", () => {
+    preview.style.backgroundImage = "none";
+});
